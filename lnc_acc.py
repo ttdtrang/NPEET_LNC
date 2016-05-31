041acabae0404d7ee5085ae9f105879451e0d98f
Trang Tran <trang@neurocan>
2016-05-30 18:01:16 -0600

    modifying mi_LNC

--- lnc_acc.py
+++ lnc_acc.py
@@ -25,65 +25,36 @@ class MI:
         return [sum(sublist,[]) for sublist in zip(*args)]
 
     @staticmethod
-    def avgdigamma(points,dvec):
-         #This part finds number of neighbors in some radius in the marginal space
-        #returns expectation value of <psi(nx)>
-        N = len(points)
-        tree = ss.cKDTree(points)
-        avg = 0.
-        for i in range(N):
-              dist = dvec[i]
-              #subtlety, we don't include the boundary point,
-              #but we are implicitly adding 1 to kraskov def bc center point is included
-              num_points = len(tree.query_ball_point(points[i],dist-1e-15,p=float('inf')))
-              avg += digamma(num_points)/N
-        return avg
+    def avgdigamma(points,dvec,metric='minkowski',p=float('inf')):
+        tree = KDTree(points, metric=DistanceMetric.get_metric(metric,p=p))
+        num_points = tree.query_radius(points, dvec - 1e-15, count_only=True)
+        return np.sum(digamma(num_points) / len(points) )
 
     @staticmethod
-    def mi_Kraskov(X,k=5,base=np.exp(1),intens=1e-10):
+
+    def mi_Kraskov(x,y,k=5,base=np.exp(1),intens=1e-10,metric="minkowski",p=np.float64('inf')):
         '''The mutual information estimator by Kraskov et al.
-
+           Inputs are 2D arrays, with each column being a dimension and each row being a data point
         '''
-        #adding small noise to X, e.g., x<-X+noise
-        x = [];
-        for i in range(len(X)):
-            tem = [];
-            for j in range(len(X[i])):
-                tem.append([X[i][j] + intens*nr.rand(1)[0]]);
-            x.append(tem);
+        assert len(x)==len(y), "Lists should have same length"
+        assert k <= len(x) - 1, "Set k smaller than num. samples - 1"
+        x +=  intens*nr.rand(x.shape[0],x.shape[1])
+        y +=  intens*nr.rand(x.shape[0],x.shape[1])
+        points = np.hstack((x,y))
 
-        points = [];
-        for j in range(len(x[0])):
-            tem = [];
-            for i in range(len(x)):
-                tem.append(x[i][j][0]);
-            points.append(tem);
-        tree = ss.cKDTree(points);
-        dvec = [];
-        for i in range(len(x)):
-            dvec.append([])
-        for point in points:
-            #Find k-nearest neighbors in joint space, p=inf means max norm
-            knn = tree.query(point,k+1,p=float('inf'));
-            points_knn = [];
-            for i in range(len(x)):
-                dvec[i].append(float('-inf'));
-                points_knn.append([]);
-            for j in range(k+1):
-                for i in range(len(x)):
-                    points_knn[i].append(points[knn[1][j]][i]);
+        #Find nearest neighbors in joint space, p=inf means max-norm
+        tree = KDTree(points, metric=DistanceMetric.get_metric(metric,p=p))
+        try:
+          dvec = tree.query(points,k+1)[0][:,k]   # no need to reshape with new query_radius method
+        except ValueError:
+          return (float("NaN"))
 
-            #Find distances to k-nearest neighbors in each marginal space
-            for i in range(k+1):
-                for j in range(len(x)):
-                    if dvec[j][-1] < fabs(points_knn[j][i]-points_knn[j][0]):
-                        dvec[j][-1] =  fabs(points_knn[j][i]-points_knn[j][0]);
-
-        ret = 0.
-        for i in range(len(x)):
-            ret -= MI.avgdigamma(x[i],dvec[i]);
-        ret += digamma(k) - (float(len(x))-1.)/float(k) + (float(len(x))-1.) * digamma(len(x[0]));
-        return ret;
+        a = avgdigamma(x,dvec*x.shape[1]/points.shape[1],metric=metric,p=p)
+        b = avgdigamma(y,dvec*y.shape[1]/points.shape[1],metric=metric,p=p)
+        c = digamma(k)
+        d = digamma(len(x))
+        # print("ee_acc: %s, %s, %s, %s" %( a,b,c,d))
+        return (-a-b+c+d)/np.log(base)
 
     @staticmethod
     def mi_LNC(x,y,k=5,base=np.exp(1),alpha=0.25,intens = 1e-10):
@@ -99,14 +70,14 @@ class MI:
         x +=  intens*nr.rand(x.shape[0],x.shape[1])
         y +=  intens*nr.rand(x.shape[0],x.shape[1])
         points = np.hstack((x,y))
- 
+
         tree = KDTree(points, metric=DistanceMetric.get_metric("minkowski",p=np.float64('inf') ))
         try:
           dvec = tree.query(points,k+1)[0][:,k]   # no need to reshape with new query_radius method
         except ValueError:
-          return (float("NaN")) 
- 
-        a,b,c,d = MI.avgdigamma(x,dvec), MI.avgdigamma(y,dvec), digamma(k), digamma(len(x)) 
+          return (float("NaN"))
+
+        a,b,c,d = MI.avgdigamma(x,dvec), MI.avgdigamma(y,dvec), digamma(k), digamma(len(x))
         # print("ee_acc: %s, %s, %s, %s" %( a,b,c,d))
         ret = (-a-b+c+d)/np.log(base)
 
@@ -116,50 +87,45 @@ class MI:
           for point in points:
             tot += 1;
             #Find k-nearest neighbors in joint space, p=inf means max norm
-            knn = tree.query(point,k+1,p=float('inf'));
-            knn_points = [];
-            for i in range(k+1):
-                tem = [];
-                for j in range(len(point)):
-                    tem.append(points[knn[1][i]][j]);
-                knn_points.append(tem);
-
-
-            #Substract mean    of k-nearest neighbor points
-            for i in range(len(point)):
-                avg = knn_points[0][i];
-                for j in range(k+1):
-                    knn_points[j][i] -= avg;
+            knn_points = points[tree.query(points,k+1,p=float('inf'))[1],:]
+            knn_points = knn_points - knn_points[0,:]
+            covr = np.outer(knn_points[1:,:].T, knn_points[1:,:]) / k
+            w,v = la.eig(covr)
+            #Substract mean of k-nearest neighbor points (For each dimension, shift every points such as the center (0-distance) is at 0
+            # for i in range(len(point)):
+            #     avg = knn_points[0][i];
+            #     for j in range(k+1):
+            #         knn_points[j][i] -= avg;
 
             #Calculate covariance matrix of k-nearest neighbor points, obtain eigen vectors
-            covr = [];
-            for i in range(len(point)):
-                tem = 0;
-                covr.append([]);
-                for j in range(len(point)):
-                    covr[i].append(0);
-            for i in range(len(point)):
-                for j in range(len(point)):
-                    avg = 0.
-                    for ii in range(1,k+1):
-                        avg += knn_points[ii][i] * knn_points[ii][j] / float(k);
-                    covr[i][j] = avg;
-            w, v = la.eig(covr);
+            # covr = [];
+            # for i in range(len(point)):
+            #     tem = 0;
+            #     covr.append([]);
+            #     for j in range(len(point)):
+            #         covr[i].append(0);
+            # for i in range(len(point)):
+            #     for j in range(len(point)):
+            #         avg = 0.
+            #         for ii in range(1,k+1):
+            #             avg += knn_points[ii][i] * knn_points[ii][j] / float(k);
+            #         covr[i][j] = avg;
+            # w, v = la.eig(covr);
 
 
             #Calculate PCA-bounding box using eigen vectors
-            V_rect = 0;
-            cur = [];
-            for i in range(len(point)):
-                maxV = 0.
-                for j in range(0,k+1):
-                    tem = 0.;
-                    for jj in range(len(point)):
-                        tem += v[jj,i] * knn_points[j][jj];
-                    if fabs(tem) > maxV:
-                        maxV = fabs(tem);
-                cur.append(maxV);
-                V_rect = V_rect + log(cur[i]);
+            # V_rect = 0;
+            # cur = [];
+            # for i in range(len(point)):
+            #     maxV = 0.
+            #     for j in range(0,k+1):
+            #         tem = 0.;
+            #         for jj in range(len(point)):
+            #             tem += v[jj,i] * knn_points[j][jj];
+            #         if fabs(tem) > maxV:
+            #             maxV = fabs(tem);
+            #     cur.append(maxV);
+            #     V_rect = V_rect + log(cur[i]);
 
             #Calculate the volume of original box
             log_knn_dist = 0.;
